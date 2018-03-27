0ad001fc6798f9f5deba53055f83489247d7361e
YDSS <y.dsheng10@gmail.com>
2018-03-27 23:36:39 +0800

    implement queue

---
+++ data_structure/queue/SequenceQueue.js
@@ -0,0 +1,66 @@
+/**
+ * @file implementation of circular sequence queue
+ *  there wiil be lack of one space compare with maxSize in circular sequence queue
+ * @author YDSS
+ *
+ * Created on Tue Mar 27 2018
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2018 YDSS
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+module.exports = class SequenceQueue {
+    constructor(maxSize) {
+        if (!maxSize) {
+            throw new Error("maxSize can not be null");
+        }
+
+        this.maxSize = maxSize;
+        this._queue = [];
+        this.front = 0;
+        this.rear = 0;
+    }
+
+    isEmpty() {
+        return this.front === this.rear;
+    }
+
+    isFull() {
+        return (this.rear + 1) % this.maxSize === this.front;
+    }
+
+    enter(item) {
+        if (this.isFull()) {
+            throw new Error('the queue is full');
+        }
+
+        this._queue[this.rear] = item;
+        this.rear = ( this.rear + 1 ) % this.maxSize;
+    }
+
+    leave() {
+        if (this.isEmpty()) {
+            throw new Error('the queue is empty');
+        }
+
+        let item = this._queue[this.front];
+        delete this._queue[this.front];
+        this.front = ( this.front + 1 ) % this.maxSize;
+
+        return item;
+    }
+}
---
+++ data_structure/queue/test.js
@@ -0,0 +1,23 @@
+/**
+ * @file test case
+ * @author YDSS
+ *
+ * Created on Tue Mar 27 2018
+ */
+
+const { expect } = require('chai');
+const SequenceQueue = require('./SequenceQueue');
+
+test('enter and leave', () => {
+    let queue = new SequenceQueue(2);
+    expect(queue.front).to.equal(0);
+    expect(queue.rear).to.equal(0);
+
+    queue.enter(123);
+    expect(queue.isFull()).to.be.true;
+    expect(() => queue.enter(23)).to.throw('the queue is full');
+
+    expect(queue.leave()).to.equal(123);
+    expect(queue.isEmpty()).to.be.true;
+    expect(() => queue.leave()).to.throw('the queue is empty');
+})
