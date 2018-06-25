fe4088d21bc606356d472b92443e780068acd7e5
YDSS <y.dsheng10@gmail.com>
2018-06-25 13:03:51 +0800

    prime

---
+++ math/prime.js
@@ -0,0 +1,40 @@
+/**
+ * funcs relating to prime
+ * 
+ * @author YDSS
+ */
+
+function isPrime(x) {
+    let sqrtX = Math.ceil(Math.sqrt(x));
+    let i = 2;    
+    let flag = true;
+
+    while(i <= sqrtX) {
+        if (Number.isInteger(x / i)) {
+            flag = false; 
+            break;
+        }
+
+        i++;
+    }
+
+    return flag;
+}
+
+/**
+ * get the smallest prime larger than x 
+ * @param {*} x 
+ */
+function nextPrime(x) {
+    let ret = x;
+    while(!isPrime(ret)) {
+        ret++;
+    }
+
+    return ret;
+}
+
+function test() {
+    console.log(nextPrime(2));
+}
+test();
