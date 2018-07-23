d7fb54e7b8eb83eec65212a64d479c193a8db322
YDSS <y.dsheng10@gmail.com>
2018-07-23 21:02:56 +0800

    update merge

--- data_structure/heap/BinomialQueue.ts
+++ data_structure/heap/BinomialQueue.ts
@@ -14,7 +14,7 @@ export default class BinomialQueue {
      */
     private queue: SiblingTreeNode[];
     /**
-     * current trees number in the queue
+     * current tree node number in the queue
      */
     size: number;
     /**
@@ -28,8 +28,15 @@ export default class BinomialQueue {
     }
 
     public merge(q: BinomialQueue): void {
+        if (this.size + q.size > this.maxSize) {
+            throw new Error("exceed the max size of queue after merged");
+        }
+
         let carry = null;
-        
+        this.size += q.size;
+        for (let i = 0, j = 1; j <= this.size; i++, j *= 2) {
+
+        }        
     }
 
     public insert(data: any): any {
