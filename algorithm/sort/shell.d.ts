18014ccefec27d85c41d6e4433eab7d167cb9301
YDSS <y.dsheng10@gmail.com>
2018-08-29 23:06:45 +0800

    finish shell sort

---
+++ algorithm/sort/shell.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @file implementation of shell sort,
+ *  use half it's length as increment
+ * @author YDSS
+ */
+declare function shell(arr: number[]): number[];
+declare let mock: number[];
--- algorithm/sort/shell.js
+++ algorithm/sort/shell.js
@@ -3,31 +3,25 @@
  *  use half it's length as increment
  * @author YDSS
  */
-
-function shell(arr: number[]): number[] {
+function shell(arr) {
     let len = arr.length;
-
-    for (
-        let increment = Math.floor(len / 2);
-        increment > 0;
-        increment = Math.floor(increment / 2)
-    ) {
+    for (let increment = Math.floor(len / 2); increment > 0; increment = Math.floor(increment / 2)) {
         for (let i = increment; i < len; i++) {
             let tmp = arr[i];
             let j;
             for (j = i; j >= increment; j -= increment) {
                 if (tmp < arr[j - increment]) {
                     arr[j] = arr[j - increment];
-                } else {
+                }
+                else {
                     break;
                 }
             }
             arr[j] = tmp;
         }
     }
-
     return arr;
 }
-
-let mock: number[] = [3, 5, 2, 1, 55, 23, 12, 35, 65];
+let mock = [3, 5, 2, 1, 55, 23, 12, 35, 65];
 console.log(shell(mock));
+//# sourceMappingURL=shell.js.map
