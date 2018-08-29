d4638230af5c735323e67220b40323a682d93061
YDSS <y.dsheng10@gmail.com>
2018-08-29 21:42:51 +0800

    shell sort, not finished

---
+++ algorithm/sort/shell.ts
@@ -0,0 +1,12 @@
+/**
+ * @file implementation of shell sort
+ * @author YDSS
+ */
+
+function shell(arr: number[]): number[] {
+    let len = arr.length;
+    let increment = Math.ceil(len / 2);
+    
+    for (; increment > 0; increment = Math.ceil(increment / 2)) {
+    }
+}
