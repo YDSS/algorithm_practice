189712ca850d85daa3d250fa850589fbd99ca756
YDSS <y.dsheng10@gmail.com>
2019-04-09 23:43:33 +0800

    finish longest substring, with poor performance, need improve!

--- leetcode/longestSubstring.js
+++ leetcode/longestSubstring.js
@@ -24,7 +24,7 @@ function lengthOfLongestSubstring(str) {
     // check if the current char is repeat
     let isRepeat = i => {
         let char = str[i];
-        if (repeatMap[char]) {
+        if (repeatMap[char] || repeatMap[char] === 0) {
             return true;
         }
 
@@ -43,9 +43,18 @@ function lengthOfLongestSubstring(str) {
             // reset the head pointer to the index of first repeat char, which is the start of new str
             let repeatCharIndex = repeatMap[str[i]];
             p = repeatCharIndex + 1;
-            // reset the map when the substring has a repeat char
-            // repeatMap = {};
-            // repeatMap[str[i]] = true;
+            // update the index of the repeat char cause we have skipped over it
+            repeatMap[str[i]] = i;
+            // clean charactors those are not in the substring but in the repeat map
+            for (let j = 0; j < p; j++) {
+                if (str[j] !== str[i]) {
+                    delete repeatMap[str[j]];
+                }                
+            }
+            // add charactors those in the substring maybe deleted incorrectly
+            for (let j = p; j < i; j++) {
+                repeatMap[str[j]] = j;
+            }
         }
         // calcuate the lenght of current substring when cursor reach the end
         else if (i === str.length - 1) { 
@@ -63,10 +72,12 @@ function lengthOfLongestSubstring(str) {
     return len;
 }
 
-// let str = 'abcabcbb';
+let str = 'abcabcbb';
 // let str = 'bbbb';
 // let str = 'bbabb';
 // let str = 'pwwkew';
 // let str = 'abcdabef';
-let str = 'dvdf';
+// let str = 'dvdf';
+// let str = "tmmzuxt";
+// let str = "tmcamzucinzxt";
 console.log(lengthOfLongestSubstring(str));
