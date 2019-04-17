6d6b624e0cb14bd794d8c1ce0afe316fd7f8ab12
YDSS <y.dsheng10@gmail.com>
2019-04-18 00:03:01 +0800

    longest palindrome substring by tracing back

--- leetcode/longestPalindrome.js
+++ leetcode/longestPalindrome.js
@@ -1,87 +1,95 @@
 /**
- * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
- *
- * @param {string} s
+ * @file Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ * @author
+ */
+
+/**
+ * DP
+ * f(i, j) = f(i + 1, j - 1) && str[i] === str[j]
+ * @param {string} str
  * @return {string}
  */
 function longestPalindrome(str) {
-    // current pointer of the traversing
-    let p = 1;
-    // head and tail of the current longest palindromic substring in s
-    let h = 0;
-    let k = 0;
     let n = str.length;
-    let maxLen = 0;
-
-    let isCharExisted = i => {
-        return str[i] != null;
-    };
-
-    if (n < 2) {
-        return "";
+    // init two dimension array
+    let stat = new Array(n); 
+    for (let i = 0; i < n; i++) {
+        stat[i] = new Array(n);
+    }
+    for (let i = 0; i < n; i++) {
+        stat[i][i] = true;
     }
 
-    for (; p < n; p++) {
-        let symcenterChar = str[p];
-        console.log(`symcenter char: ${symcenterChar}`);
-
-        // situation 1: length of palindromic string is odd, that means symcenter is one char
-        if (
-            isCharExisted(p - 1) &&
-            isCharExisted(p + 1) &&
-            str[p - 1] === str[p + 1]
-        ) {
-            let i = p - 2; // traverse to left to expand the palindromic string
-            let j;
-            for (; i >= 0; i--) {
-                j = p + p - i; // symmetry
-                console.log(`i: ${i}, j: ${j}`);
-
-                if (!isCharExisted(str[j]) || str[i] !== str[j]) {
-                    console.log(`the substring: ${str.slice(i + 1, j)}`);
-                    if (j - i - 1 > maxLen) {
-                        h = i + 1;
-                        k = j - 1;
-                        maxLen = j - i - 1;
-                    }
-
-                    break;
-                }
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            if (stat[i + 1][j - 1] && str[i] === str[j]) {
+                stat[i][j] = true;
             }
-        }
-        // situation 2: length of palindromic string is odd, that means symcenter is two same chars
-        else if (
-            (isCharExisted(p - 1) && str[p - 1] === symcenterChar) ||
-            (isCharExisted(p + 1) && str[p + 1] === symcenterChar)
-        ) {
-            let i;
-            let j;
-            // prepare for next round
-            if (isCharExisted(p - 1) && str[p - 1] === symcenterChar) {
-                i = p - 2;
-                j = p + 1;
-            } else {
-                i = p - 1;
-                j = p + 2;
-            }
-
-            while (isCharExisted(i) && isCharExisted(j) && str[i] === str[j]) {
-                i--;
-                j++;
-            }
-
-            if (j - i + 1 > maxLen) {
-                h = i;
-                k = j;
-                maxLen = j - i + 1;
-            }
-        }
+        } 
     }
-
-    return str.slice(h, k + 1);
 }
 
+/**
+ * tracing back
+ * @param {string} s
+ * @return {string}
+ */
+function longestPalindrome2(str) {
+    let n = str.length;
+    let maxLen = 0;
+    let h, k;
+
+    let isPalindromeStr = (left, right) => {
+        if (left === right) {
+            return true;
+        }
+
+        let i = left;
+        let j = right;
+        let ret = true;
+
+        while (i <= j) {
+            if (str[i] !== str[j]) {
+                ret = false;
+                break;
+            }
+
+            i++;
+            j--;
+        }
+
+        return ret;
+    }
+
+    getMaxLen(0, 0);
+    console.log(`maxLen: ${maxLen}`);
+    return str.slice(h, k + 1);
+
+    function getMaxLen(i, j) {
+        if (i > j) {
+            return;
+        }
+        if (i >= n || j >= n) {
+            return;
+        }
+
+        getMaxLen(i + 1, j);
+        getMaxLen(i, j + 1);
+        if (isPalindromeStr(i, j)) {
+            let thisLen = j - i + 1;
+            if (thisLen > maxLen) {
+                h = i;
+                k = j;
+                maxLen = thisLen;
+            }
+        }
+    }
+}
+
+// let str = "";
+// let str = "a";
 // let str = "babad";
-let str = "abb";
+// let str = "abb";
 // let str = "dacbbcalfa";
+let str = "abcdasdfghjkldbcba";
 console.log(longestPalindrome(str));
