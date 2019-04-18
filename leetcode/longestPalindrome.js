0572f601fac068c0c3f6428d8857a8921e0f0ca0
YDSS <y.dsheng10@gmail.com>
2019-04-18 21:54:18 +0800

    nailed longest palindrome by mirror char thought

--- leetcode/longestPalindrome.js
+++ leetcode/longestPalindrome.js
@@ -2,6 +2,65 @@
  * @file Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
  * @author
  */
+/**
+ * find the center char of a palindrome substring, which is the symetry of the str
+ * T(n) = O(n^2) S(n) = O(1)
+ * 
+ * @param {string} str
+ * @return {string}
+ */
+function longestPalindrome3(str) {
+    let n = str.length;
+    let maxLen = 0;
+    let p;
+    let i, j;
+    let h, k; // head and tail pointer of the current longest palindrome substring
+
+    let isCharExisted = i => {
+        return str[i] != null;
+    }
+
+    // find the only one char center
+    for (p = 0; p < n; p++) { // O(n)
+        i = p;
+        j = p;
+
+        while (isCharExisted(i - 1) && isCharExisted(j + 1) && str[i - 1] === str[j + 1]) { // Tmax = O(n)
+            i--;
+            j++;
+        }
+        // console.log(`one i: ${i}, j: ${j}`);
+        let len = j - i + 1;
+        if (len > maxLen) {
+            h = i;
+            k = j;
+            maxLen = len;
+        }
+    }
+
+    // find two chars center
+    for (p = 0; p < n; p++) { // O(n - 1)
+        i = p;
+        j = p + 1;
+
+        if (str[i] === str[j]) { // means this two char can be the mirror
+            while (isCharExisted(i - 1) && isCharExisted(j + 1) && str[i - 1] === str[j + 1]) { // Tmax = O(n)
+                i--;
+                j++;
+            }
+            // console.log(`two i: ${i}, j: ${j}`);
+            let len = j - i + 1;
+            if (len > maxLen) {
+                h = i;
+                k = j;
+                maxLen = len;
+            }
+        }
+    }
+
+    // console.log(`maxLen: ${maxLen}`);
+    return str.slice(h, k + 1);
+}
 
 /**
  * DP
@@ -91,5 +150,5 @@ function longestPalindrome2(str) {
 // let str = "babad";
 // let str = "abb";
 // let str = "dacbbcalfa";
-let str = "abcdasdfghjkldbcba";
+// let str = "abcdasdfghjkldbca";
 console.log(longestPalindrome(str));
