c577375c283c232d96c65532f67f5cf4c2ea376d
YDSS <y.dsheng10@gmail.com>
2019-05-03 14:33:10 +0800

    three sum

--- leetcode/3sum.js
+++ leetcode/3sum.js
@@ -8,7 +8,7 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-function threeSum(nums) {
+function threeSum2(nums) {
     let solutions = [];
     let n = nums.length;
     for (let i = 0; i < n; i++) {
@@ -35,15 +35,14 @@ function threeSum(nums) {
                                 sMap[num] = 0;
                             }
                             sMap[num]++;
-                        })
+                        });
                         // console.log(sMap)
                         let innerFlag = false;
                         for (let m = 0; m < newSol.length; m++) {
                             if (!sMap[newSol[m]] || sMap[newSol[m]] <= 0) {
                                 innerFlag = true;
                                 break;
-                            }
-                            else {
+                            } else {
                                 sMap[newSol[m]]--;
                             }
                         }
@@ -63,6 +62,49 @@ function threeSum(nums) {
     return solutions;
 }
 
+/**
+ * hashtable solution, T(n) = O(i * j), i + j = n,
+ * S(n) = O(n)
+ * @param {Array} nums
+ */
+function threeSum(nums) {
+    let solutions = [];
+    // init hashtable
+    let table = {};
+    nums.map(num => { // O(n)
+        if (table[num] == null) {
+            table[num] = 0;
+        }
+        table[num]++;
+    });
+    // check if there are at least 3 zeros, which is a solution
+    if (table[0] > 2) {
+        solutions.push([0, 0, 0]);
+    }
+    // init arr contains positive nums and negative nums
+    let pos = [];
+    let neg = [];
+    Object.keys(table).map(num => { // O(n)
+        num = +num; // string to num, cause object's key is string
+        num >= 0 && pos.push(num);
+        num < 0 && neg.push(num);
+    });
+    for (let i = 0; i < pos.length; i++) { // i + j = n; O(i * j)
+        for (let j = 0; j < neg.length; j++) { // O(j)
+            let dif = 0 - pos[i] - neg[j];
+            if ((dif === pos[i] || dif === neg[j]) && table[dif] > 1) {
+                solutions.push([pos[i], dif, neg[j]]);
+            }
+            if ((dif > pos[i] || dif < neg[j]) && table[dif] > 0) {
+                solutions.push([pos[i], dif, neg[j]]);
+            }
+        }
+    }
+
+    return solutions;
+}
+
 // let arr = [-1, 0, 1, 2, -1, -4]
-let arr = [-13,5,13,12,-2,-11,-1,12,-3,0,-3,-7,-7,-5,-3,-15,-2,14,14,13,6,-11,-11,5,-15,-14,5,-5,-2,0,3,-8,-10,-7,11,-5,-10,-5,-7,-6,2,5,3,2,7,7,3,-10,-2,2,-12,-11,-1,14,10,-9,-15,-8,-7,-9,7,3,-2,5,11,-13,-15,8,-3,-7,-12,7,5,-2,-6,-3,-10,4,2,-5,14,-3,-1,-10,-3,-14,-4,-3,-7,-4,3,8,14,9,-2,10,11,-10,-4,-15,-9,-1,-1,3,4,1,8,1]
+let arr = [3, 0, -2, -1, 1, 2];
+// let arr = [-13,5,13,12,-2,-11,-1,12,-3,0,-3,-7,-7,-5,-3,-15,-2,14,14,13,6,-11,-11,5,-15,-14,5,-5,-2,0,3,-8,-10,-7,11,-5,-10,-5,-7,-6,2,5,3,2,7,7,3,-10,-2,2,-12,-11,-1,14,10,-9,-15,-8,-7,-9,7,3,-2,5,11,-13,-15,8,-3,-7,-12,7,5,-2,-6,-3,-10,4,2,-5,14,-3,-1,-10,-3,-14,-4,-3,-7,-4,3,8,14,9,-2,10,11,-10,-4,-15,-9,-1,-1,3,4,1,8,1]
 console.log(threeSum(arr));
