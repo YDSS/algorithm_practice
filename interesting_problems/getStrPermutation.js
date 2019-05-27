1d17d01b361c61a68b2a5524aa11fb63d43684fd
YDSS <y.dsheng10@gmail.com>
2019-05-28 00:01:15 +0800

    str permutation, not finish

---
+++ interesting_problems/getStrPermutation.js
@@ -0,0 +1,32 @@
+/**
+ * @file get all permutation of a string, assume that no duplicates in the string
+ * @author arlenyang
+ */
+
+function getStrPermutation(str) {
+    let permutation = [];
+    let recursion = (i) => {
+        if (i === 0) {
+            permutation.push(str[i]);
+            return;
+        }
+
+        recursion(i - 1, permutation);
+        console.log(permutation);
+        console.log(str[i])
+        // insert str[i] to all the current permutation
+        let tmp = [];
+        for (let j = 0; j < permutation.length; j++) {
+            let p = permutation[j];
+            for (let k = 0; k < p.length; k++) {
+                tmp.push(p.slice(0, k) + str[i] + p.slice(k));
+            }
+        }
+        permutation = tmp;
+    }
+    recursion(str.length - 1);
+    return permutation;
+}
+
+let str = 'abc';
+console.log(getStrPermutation(str));
