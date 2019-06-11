9ed60a25688c7068f35d0c41f82c855e0a0b5fbd
YDSS <y.dsheng10@gmail.com>
2019-06-11 19:27:07 +0800

    solve sudoku by backtracing with hashtable

--- leetcode/solveSudoku.js
+++ leetcode/solveSudoku.js
@@ -17,20 +17,20 @@ function solveSudoku(board) {
     // init tables
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
+            if (!rowTable[i]) {
+                rowTable[i] = new Set();
+            }
+            if (!columnTable[j]) {
+                columnTable[j] = new Set();
+            }
+            let boxIndex = getBoxIndex(i, j);
+            if (!boxTable[boxIndex]) {
+                boxTable[boxIndex] = new Set();
+            }
             let val = board[i][j];
             if (val !== ".") {
-                if (!rowTable[i]) {
-                    rowTable[i] = new Set();
-                }
                 rowTable[i].add(val);
-                if (!columnTable[j]) {
-                    columnTable[j] = new Set();
-                }
                 columnTable[j].add(val);
-                let boxIndex = getBoxIndex(i, j);
-                if (!boxTable[boxIndex]) {
-                    boxTable[boxIndex] = new Set();
-                }
                 boxTable[boxIndex].add(val);
             }
         }
@@ -54,7 +54,7 @@ function solveSudoku(board) {
     let findNextEmptyCeil = (row, column) => {
         let start = row;
         for (let i = row; i < 9; i++) {
-            for (let j = (start === i ? column : 0); j < 9; j++) {
+            for (let j = start === i ? column : 0; j < 9; j++) {
                 if (board[i][j] === ".") {
                     return {
                         i,
@@ -64,63 +64,64 @@ function solveSudoku(board) {
             }
         }
 
-        return null
+        return null;
     };
-    let found = false;
     let bt = (row, column) => {
-        if (found) {
-            return;
-        }
         // find next empty ceil from (row, column)
         let ceil = findNextEmptyCeil(row, column);
         if (ceil == null) {
-            found = true;
-            return;
+            return true;
         }
         let { i, j } = ceil;
         let nums = getValidNums(i, j);
         if (nums.length === 0) {
-            return;
+            return false;
         }
-        nums.map(num => {
+        for (let k = 0; k < nums.length; k++) {
+            let num = nums[k];
             let boxIndex = getBoxIndex(i, j);
             // add num into tables
             rowTable[i].add(num);
             columnTable[j].add(num);
             boxTable[boxIndex].add(num);
             board[i][j] = num;
-            console.log(i, j, num, nums)
-            console.log(rowTable[i])
-            console.log(columnTable[j])
-            console.log(boxTable[boxIndex])
             // fill next possible num
-            bt(i, j);
-            if (found) {
-                return;
+            if (bt(i, j)) {
+                return true;
             }
             // restore changes before
             rowTable[i].delete(num);
             columnTable[j].delete(num);
             boxTable[boxIndex].delete(num);
             board[i][j] = ".";
-        });
-        // if (i === 8 && j === 8) {
-        //     found = true;
-        // }
+        }
+
+        return false;
     };
     bt(0, 0);
 }
 
+// let board = [
+//     ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+//     ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+//     [".", "9", "8", ".", ".", ".", ".", "6", "."],
+//     ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+//     ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+//     ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+//     [".", "6", ".", ".", ".", ".", "2", "8", "."],
+//     [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+//     [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+// ];
 let board = [
-    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
-    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-    [".", "9", "8", ".", ".", ".", ".", "6", "."],
-    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-    [".", "6", ".", ".", ".", ".", "2", "8", "."],
-    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+    [".", ".", "9", "7", "4", "8", ".", ".", "."],
+    ["7", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", "2", ".", "1", ".", "9", ".", ".", "."],
+    [".", ".", "7", ".", ".", ".", "2", "4", "."],
+    [".", "6", "4", ".", "1", ".", "5", "9", "."],
+    [".", "9", "8", ".", ".", ".", "3", ".", "."],
+    [".", ".", ".", "8", ".", "3", ".", "2", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", "6"],
+    [".", ".", ".", "2", "7", "5", "9", ".", "."]
 ];
 solveSudoku(board);
 console.log(board);
