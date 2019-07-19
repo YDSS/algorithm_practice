9d162e0871ae5d41dac7bab4c2945b2e4d1f8d24
YDSS <y.dsheng10@gmail.com>
2019-07-19 15:02:01 +0800

    review median of two sorted arrays

--- leetcode/findMedianSortedArrays.js
+++ leetcode/findMedianSortedArrays.js
@@ -85,6 +85,97 @@ function findMedianSortedArrays(A, B) {
     }
 }
 
+function findMedianSortedArrays2(A, B) {
+    // swap A,B if B is shorter, make sure always using shorter array to binary search
+    if (A.length > B.length) {
+        let tmp = A;
+        A = B;
+        B = A;
+    }
+
+    let m = A.length;
+    let n = B.length;
+    let isTotalEven = isEven(m + n);
+    let half = Math.ceil((m + n) / 2);
+    let low = Math.max(0, half - n); // min amount of A split into left of (A U B)
+    let high = Math.min(half, m); // max amount of A split into left of (A U B)
+    let leftNumOfA;
+
+    // we can always find the right combination
+    while (low <= high) {
+        // binary search for pivot of index of left part of A
+        leftNumOfA = Math.floor((low + high) / 2); // amount of A split into left of (A U B)
+
+        // notice that if leftNumOfA is 0, a can be undefined, which means logic below is not right,
+        // but it's ok because undefined == 0 when compare to other number
+        // and when it comes to 0, which means it's the last solution, we know it's always a combination,
+        // so that must it, and the entire loop ended
+        let a = A[leftNumOfA - 1]; // last elem in left part of A
+        let ap = A[leftNumOfA]; // elem next to a
+        let b = B[half - leftNumOfA - 1]; // last elem in left part of B
+        let bp = B[half - leftNumOfA]; // elem next to b
+        if (a >= b) {
+            if (bp != null && a <= bp) {
+                // found the right combination
+                break;
+            } else {
+                // left of A is too much
+                high = leftNumOfA - 1;
+            }
+        } else {
+            if (ap != null && b <= ap) {
+                // found
+                break;
+            } else {
+                // left of A is too litte
+                low = leftNumOfA + 1;
+            }
+        }
+
+        // or
+    }
+
+    let medianOfLeft;
+    let lastLeftOfA = A[leftNumOfA - 1];
+    let lastLeftOfB = B[half - leftNumOfA - 1];
+    if (lastLeftOfA == null) {
+        medianOfLeft = lastLeftOfB;
+    }
+    else if (lastLeftOfB == null) {
+        medianOfLeft = lastLeftOfA;
+    }
+    else {
+        medianOfLeft = Math.max(A[leftNumOfA - 1], B[half - leftNumOfA - 1]);
+    }
+
+    if (!isTotalEven) {
+        // odd, return median directly
+        return medianOfLeft;
+    } else {
+        // even, need find the median of right part,
+        // which is the smaller one of the first elem from A and the first elem from B,
+        // still need consider if any of them is null
+        let medianOfRight;
+        let firstRightOfA = A[leftNumOfA];
+        let firstRightOfB = B[half - leftNumOfA];
+        if (firstRightOfA == null) {
+            medianOfRight = firstRightOfB;
+        } else if (firstRightOfB == null) {
+            medianOfRight = firstRightOfA;
+        } else {
+            medianOfRight = Math.min(firstRightOfA, firstRightOfB);
+        }
+
+        return (medianOfLeft + medianOfRight) / 2;
+    }
+}
+
+function isEven(num) {
+    let half = num / 2;
+    return half === Math.floor(half);
+}
+
+
 // let A = [1, 2, 3, 6];
 // let B = [4];
 let A = [];
